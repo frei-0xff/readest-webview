0c8df56f62f89a6f6a22eaf291f40924a91c848f
frei-0xff <frei.0xff@gmail.com>
2026-07-12 21:54:51 +0300

    Revert "GeckoView backround color fix"
    This reverts commit 69ede96b4d2b73183fa7414f5a552fa33479e2bb.

--- app/src/main/java/io/github/frei0xff/readestwebview/MainActivity.kt
+++ app/src/main/java/io/github/frei0xff/readestwebview/MainActivity.kt
@@ -3,7 +3,6 @@ package io.github.frei0xff.readestwebview
 import android.app.AlertDialog
 import android.content.SharedPreferences
 import android.graphics.Rect
-import android.graphics.Color
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
@@ -87,7 +86,6 @@ class MainActivity : AppCompatActivity() {
         }
 
         geckoView = GeckoView(this)
-        geckoView.setBackgroundColor(Color.BLACK)
         geckoView.setSession(session)
         setContentView(geckoView)
 
